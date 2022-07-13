ec2273177d27353f006586f39bf39051e94fcc32
Andrejs <ajancevskis@gmail.com>
2022-07-13 14:54:17 +0100

    only show images saved by user

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -12,9 +12,9 @@ class ImageController extends Controller
 {
     
 
-    // Show all images TODO: only show images by user
-    public function index() {
-        $images = Image::latest()->get();
+    // Show all images saved by user
+    public function index(Request $request) {
+        $images = Image::where("user_id", $request->user()->id)->get();
         foreach ($images as $image) {
             $image["image_url"] = $this->getFetchUrl($image); // Probably better to move to google bucket
         }
@@ -23,18 +23,28 @@ class ImageController extends Controller
 
     // Show single image
     public function show(Image $image) {
-        // TODO: only show if user owns image
-        $image["image_url"] = $this->getFetchUrl($image);
+        // only show if user owns image
+        if (request()->user()->id == $image->user_id) {
+            $image["image_url"] = $this->getFetchUrl($image);
         
-        return $image;
+            return $image;
+        } else {
+            return response()->json(["error" => "Not authorized."]);
+        }
+        
     }
 
     public function fetchImage(Image $image){
-        // TODO: only show if user owns image
-        // Get image from local disk
-        $local_path = config('filesystems.disks.local.root') . DIRECTORY_SEPARATOR . $image->image_path;
+        // only show if user owns image
+        if (request()->user()->id == $image->user_id) {
+            // Get image from local disk
+            $local_path = config('filesystems.disks.local.root') . DIRECTORY_SEPARATOR . $image->image_path;
 
-        return response()->file($local_path);
+            return response()->file($local_path);
+        } else {
+            return response()->json(["error" => "Not authorized."]);
+        }
+        
     }
 
     public function store(StoreImageRequest $request){
@@ -42,8 +52,7 @@ class ImageController extends Controller
         $formFields = $request->all();
         // Store image file and save path in database
         $formFields["image_path"] = $request->file("image")->store("images");
-        // TODO: add user_id stuff
-        $formFields["user_id"] = "1";
+        $formFields["user_id"] = $request->user()->id;
  
         $image = Image::create($formFields);
         
